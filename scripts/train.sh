3471bce3159270467fe36834ab89eceebb8f3821
sunyicong2003-creator <sunyicong2003@gmail.com>
2026-05-21 18:48:18 +0200

    first commit

--- scripts/train.sh
+++ scripts/train.sh
@@ -17,13 +17,13 @@ SECONDS=0
 
 logs=$base/logs
 
-model_name=deen_transformer_regular
+model_name=transformer_post
 
 mkdir -p $logs
 
 mkdir -p $logs/$model_name
 
-OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/$model_name.yaml > $logs/$model_name/out 2> $logs/$model_name/err
+OMP_NUM_THREADS=$num_threads python3 -m joeynmt train $configs/$model_name.yaml > $logs/$model_name/out 2> $logs/$model_name/err
 
 echo "time taken:"
 echo "$SECONDS seconds"
